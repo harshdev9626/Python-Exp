d8c01b371fffe7a32da7e7ab75fd449f9356f376
HarshPowar <harshupowar9626@gmail.com>
2026-07-22 15:46:55 +0530

    Initial commit

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+str=input("Enter Your Age:")
+age=int(str)
+if age>=18:
+    print("You Are Eligible for Vote")
+else:
+    print("You Are Not Eligible for Vote")
+    
---
+++ zero_or_not.py
@@ -0,0 +1,7 @@
+# WAP that reads value of N and check the number zero or non zero
+num=int(input("Enter Number:"))
+
+if num==0:
+    print("Zero Number")
+else:
+    print("Non Zero Number")
